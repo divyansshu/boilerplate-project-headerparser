2cf810b280aa1388e93bc52c7c7b2bbd5d61a779
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2023-08-15 09:24:35 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,12 +3,19 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const requestIp = require('request-ip');
+const accepts = require("accepts");
+const userAgent = require("useragent");
+
+const app = express();
+
+//use the request-ip middleware
+app.use(requestIp.mw());
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,8 +27,20 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  
+  const ipaddress = req.clientIp;
+//parse the 'Accepts-Language' header of the request
+const acceptLanguages = accepts(req);
+  const language = acceptLanguages.language(['en', 'fr', 'es']); // Pass an array of supported languages
+
+  //parse the user-agent header of the requrest
+  const agent = userAgent.parse(req.headers['user-agent']);
+  const software = agent.toString();
+  res.json({ ipaddress: ipaddress, "language": language, software: software });
+  console.log(ipaddress);
+  console.log(language);
+  console.log(software);
 });
 
 // listen for requests :)
